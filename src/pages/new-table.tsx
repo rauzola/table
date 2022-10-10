295e8a138e91a5d8349f3274a645255233eff187
Raul Sigoli <raul_sigoli@hotmail.com>
2022-10-10 11:58:42 -0300

    adicionando colunas novas

--- src/pages/new-table.tsx
+++ src/pages/new-table.tsx
@@ -1,14 +1,14 @@
 import { NewTable } from "../components/NewTable";
 import React from "react";
 import data from '../../Lista_de_camadas_.json';
-import {IColumn } from '../components/NewTable'
+import { IColumn } from '../components/NewTable'
 
 interface ILayer {
     id: string;
     name?: string;
     description?: string;
     type?: string;
-  }
+}
 
 
 export default function newTable() {
@@ -16,20 +16,35 @@ export default function newTable() {
     const layers = data.data.layerCatalogs;
     const columns: IColumn[] = [
         {
-          title: 'Nome',
-          dataIndex: 'name',
-          key: 'name'
+            title: 'id',
+            dataIndex: 'id',
+            key: 'id'
+        },
+        {
+            title: 'Nome',
+            dataIndex: 'name',
+            key: 'name'
         },
         {
             title: 'Descrição',
             dataIndex: 'description',
             key: 'description'
-          }
+        },
+        {
+            title: 'type',
+            dataIndex: 'type',
+            key: 'type'
+        },
+        {
+            title: 'views',
+            dataIndex: 'views',
+            key: 'views'
+        }
     ];
 
-    return(
+    return (
         <>
-            <NewTable<ILayer> data={layers.items} columns={columns}/>
+            <NewTable<ILayer> data={layers.items} columns={columns} />
         </>
     )
 }
